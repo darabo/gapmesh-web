157c38c512e003c9a6aade740bedb446b4008357
Dara Bonakdar <4589296+darabo@users.noreply.github.com>
2026-03-14 21:54:21 -0400

    Update ANDROID_BETA_URL to new Play Store link

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const APP_STORE_URL = "https://apps.apple.com/us/app/gap-mesh/id6757211522";
 export const PLAY_STORE_URL = "https://play.google.com/store/apps/details?id=com.gapmesh.droid";
 export const TESTFLIGHT_URL = "https://testflight.apple.com/join/Vgbv1MTy";
-export const ANDROID_BETA_URL = "https://groups.google.com/g/gap-mesh-android";
+export const ANDROID_BETA_URL = "https://play.google.com/apps/testing/com.gapmesh.droid";
 export const GITHUB_IOS_URL = "https://github.com/darabo/gapmesh-ios/tree/main";
 export const GITHUB_ANDROID_URL = "https://github.com/darabo/gap-android-main";
 export const APK_RELEASE_URL = "https://github.com/darabo/gap-android/releases";
